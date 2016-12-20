a5c36baa6a5cabe331ab8f09f7d3ff67a3f29e2a
Bjoern <dev_mails@t-online.de>
2016-12-20 21:45:38 +0000

    Aded pug and a test template, showing on root URL.

--- server.js
+++ server.js
@@ -1,39 +1,56 @@
 const express = require("express");
-var app = express()
+require('./node_modules/express-group-routes');
+require('./node_modules/pug');
+var app = express();
+
+app.set('view engine', 'pug');
 
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 app.get("/favicon.ico", function(req, res) {
     res.send("");
 });
-app.get('/:time', function (req, res) {
-    var time = req.params.time;
-    var ret = {
-        unix: null,
-        natural: null
-    };
-    
-    var myRegexp = /^([a-zA-Z]{3,8})\s(\d{1,2}),\s(\d{4})$/;
-    var match = myRegexp.exec(time);
-    
-    if ( /*!isNan(+time) &&*/ (new Date(+time)).getTime() > 0 ) {   // if timestamp
-        var date = new Date(+time);
-        ret.natural = months[date.getMonth()] + " " + date.getDay() + ", " + date.getFullYear();
-        ret.unix = time;
-    } else if(match) {
-        var month = months.indexOf(match[1]);
-        if ( month>-1 ) {   // if the month is valid..
-            var date = new Date(+match[3], +month, +match[3]);
-            ret.unix = date.getTime();
-            ret.natural = time;
+
+app.get("/|/about", function(req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
+
+app.group("/test", (router) => {
+   router.get('/', function(req, res) {
+        res.send("lalala");   
+   });
+});
+
+app.group("/timestamp", (router) => {
+    router.get('/:time', function (req, res) {
+        var time = req.params.time;
+        var ret = {
+            unix: null,
+            natural: null
+        };
+        
+        var myRegexp = /^([a-zA-Z]{3,8})\s(\d{1,2}),\s(\d{4})$/;
+        var match = myRegexp.exec(time);
+        
+        if ( /*!isNan(+time) &&*/ (new Date(+time)).getTime() > 0 ) {   // if timestamp
+            var date = new Date(+time);
+            ret.natural = months[date.getMonth()] + " " + date.getDay() + ", " + date.getFullYear();
+            ret.unix = time;
+        } else if(match) {
+            var month = months.indexOf(match[1]);
+            if ( month>-1 ) {   // if the month is valid..
+                var date = new Date(+match[3], +month, +match[3]);
+                ret.unix = date.getTime();
+                ret.natural = time;
+            }
+        } else {
+            //ret.unix = null; ret.natural = null;
         }
-    } else {
-        //ret.unix = null; ret.natural = null;
-    }
-    res.send(ret)
-})
-app.get('/', function (req, res) {
-    res.send('Usage: /<em>timestamp</em> or /<em>natural date, e.g. January 15, 2016</em>. Replace spaces with \%20! <strong>The timestamp should be in ms!</strong>');
+        res.send(ret)
+    })
+    router.get('/', function (req, res) {
+        res.send('Usage: /<em>timestamp</em> or /<em>natural date, e.g. January 15, 2016</em>. Replace spaces with \%20! <strong>The timestamp should be in ms!</strong>');
+    });
 });
 
 app.listen(8080, function () {
