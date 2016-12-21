960c50eec1b4c07b8c57a6772ec480a208e7b170
Bjoern <dev_mails@t-online.de>
2016-12-21 12:46:01 +0000

    URL shortener working.

--- service_shorturl.js
+++ service_shorturl.js
@@ -1,5 +1,7 @@
 var express = require('express')
-  , router = express.Router()
+  , router = express.Router();
+var ObjectID = require('mongodb').ObjectID;
+var shortid = require('shortid');
 
 var dbUrl = 'mongodb://localhost:27017/urls';
 var collectionName = "test";
@@ -9,24 +11,34 @@ function loadUrl(db, url, id, callback) {
     var collection = db.collection(collectionName)
         var find = {};
         if ( url ) { find.url = url; }
-        if ( id ) { find._id = id; }
+        if ( id ) {
+            var o_id = id;
+            find._id = o_id;
+        }
+        //console.log(find);
         collection.find(find).toArray(function(err, urls) {
             if (err) throw err
             callback(urls);
     })
 }
 
-router.get('/new/:url', function (req, res) {
-    var url = req.params.url;
+//router.get('/new/:url', function (req, res) {
+router.get('/new', function (req, res) {
+    console.log(req.query.p);
+    var url = req.query.p;
+    if ( !url ) { res.send("Please specify URL as query parameter 'p'!"); return; }
+    if ( (url.match(/^https?:\/\/www\d?\.\w+(\.\w+)+(\/\w+)*\/?$/)||[]).length<1 ) { res.send("URL format has to be 'http[s]://www.&lt;host&gt;.&lt;tld&gt;[/&lt;path&gt;]'"); return; }
     mongo.connect(dbUrl, function(err, db) {
         if (err) throw err
         loadUrl(db, url, null, function(urls) {
             var obj = {
-                url: url
+                url: url,
+                _id: shortid.generate()
             };
             if ( urls.length>0 ) {
-                obj.id = urls[0]._id;
                 obj.msg = "Already existing.";
+                obj.id = urls[0]._id;
+                delete obj._id;
                 res.send(obj);
                 return false;
             }
@@ -34,7 +46,8 @@ router.get('/new/:url', function (req, res) {
             var collection = db.collection(collectionName)
             collection.insert(obj, function(err, data) {
                 if (err) throw err
-                obj.id= data.ops[0]._id;
+                obj.id = data.ops[0]._id;
+                delete obj._id;
                 db.close()
                 res.send(obj);
             })
@@ -57,9 +70,9 @@ router.get('/r/:id', function (req, res) {
         loadUrl(db, null, id, function(urls) {
             if ( urls.length>0 ) {
                 var url = urls[0];
-                res.redirect(url);
+                res.redirect(url.url);
             } else {
-                res.send({err: "Invalid ID!"});
+                res.send({err: "Invalid ID!", id: id, urls: urls});
             }
         });
     });
